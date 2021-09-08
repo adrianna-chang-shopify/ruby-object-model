4ad89b4668c5bce9504b67eab9c13b1a6645c469
Adrianna Chang <adrianna.chang@shopify.com>
2021-09-08 09:29:41 -0400

    Experiment with moving class to lib/ folder

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
-$LOAD_PATH.unshift File.expand_path("../", __dir__)
+# We don't need this anymore for Rake because it automatically adds lib/
+# to $LOAD_PATH before running the tests, but let's leave it in so we can run
+# the tests directly with Ruby
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 require "minitest/autorun"
