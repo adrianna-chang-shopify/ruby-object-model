00352a6759cce88cb7d14206834700d3cda37101
Adrianna Chang <adrianna.chang@shopify.com>
2021-10-06 13:23:55 -0400

    ToyClass class that can initialize ToyObjects using 'new'

---
+++ test/toy_class_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+require 'toy_class'
+require 'toy_object'
+
+[[Class, Object], [ToyClass, ToyObject]].each do |klass, object_klass|
+  describe klass do
+    before do
+      @class = klass.new
+    end
+
+    describe 'initializing an object' do
+      specify 'creates a ToyObject instance' do
+        assert_kind_of object_klass, call_method(:new)
+      end
+    end
+
+    def call_method(name, *args)
+      name = @class.is_a?(ToyClass) ? "toy_#{name}" : name
+      @class.public_send(name, *args)
+    end
+  end
+end
