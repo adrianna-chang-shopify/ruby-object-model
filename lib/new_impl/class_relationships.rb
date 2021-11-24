d2eeca225c0702f113d21aca2cf5d30b4099f15d
Adrianna Chang <adrianna.chang@shopify.com>
2021-11-24 14:07:56 -0500

    Make object tests work with new implementation

--- lib/new_impl/class_relationships.rb
+++ lib/new_impl/class_relationships.rb
@@ -25,6 +25,43 @@ module NewImpl
   #   e.include?(klass)
   # end
 
+  def ToyObject.toy_new
+    instance = BasicObject.new
+
+    klass = self
+
+    singleton_class = (class << instance; self; end)
+    singleton_class.define_method(:toy_class) { klass }
+
+    class << instance
+      def ivar_map
+        @ivar_map ||= {}
+      end
+    
+      def toy_instance_variable_get(name)
+        ivar_map[name.to_sym]
+      end
+    
+      def toy_instance_variable_set(name, value)
+        ivar_map[name.to_sym] = value
+      end
+  
+      def toy_instance_variables
+        ivar_map.keys
+      end
+  
+      def toy_instance_variable_defined?(name)
+        ivar_map.has_key?(name.to_sym)
+      end
+    
+      def toy_remove_instance_variable(name)
+        ivar_map.delete(name.to_sym)
+      end
+    end 
+  
+    instance
+  end
+
   def ToyObject.toy_kind_of?(klass)
     superclass = toy_class
     while superclass
--- test/toy_object_test.rb
+++ test/toy_object_test.rb
@@ -1,91 +1,83 @@
 require "test_helper"
-require "toy_class"
-require "toy_object"
+require "new_impl/class_relationships"
 
-# Alternative: we could have created a new class, and delegated the missing
-# toy methods to Object's native implementation
-# class FakeToyObject
-#   def method_missing(selector, *args)
-#     selector = selector.to_s.delete_prefix("toy_")
-#     send selector, *args
-#   end
-# end
+module NewImpl
+  [ 
+    [Object, Class], [ToyObject, ToyClass, "toy_"],
+    # [Module, Class], [ToyModule, ToyClass, "toy_"],
+    # [Class, Class], [ToyClass, ToyClass, "toy_"]
+  ].each do |klass, class_klass, meth_prefix|
+    describe klass do
+      include TestHelpers
 
-[ 
-  [Object, Class], [ToyObject, ToyClass, "toy_"],
-  [Module, Class], [ToyModule, ToyClass, "toy_"],
-  [Class, Class], [ToyClass, ToyClass, "toy_"]
-].each do |klass, class_klass, meth_prefix|
-  describe klass do
-    include TestHelpers
-
-    before do
-      @object = call_method(klass, meth_prefix, :new)
-    end
-
-    describe "accessing instance variables" do
-      specify "reading and writing with a Symbol name" do
-        call_method(@object, meth_prefix, :instance_variable_set, :@foo, :bar)
-        assert_equal :bar, call_method(@object, meth_prefix, :instance_variable_get, :@foo)
-
-        call_method(@object, meth_prefix, :instance_variable_set, :@foo, :baz)
-        assert_equal :baz, call_method(@object, meth_prefix, :instance_variable_get, :@foo)
+      before do
+        @object = call_method(klass, meth_prefix, :new)
       end
 
-      specify "reading and writing with a String name" do
-        call_method(@object, meth_prefix, :instance_variable_set, "@foo", :bar)
-        assert_equal :bar, call_method(@object, meth_prefix, :instance_variable_get, "@foo")
+      describe "accessing instance variables" do
+        specify "reading and writing with a Symbol name" do
+          call_method(@object, meth_prefix, :instance_variable_set, :@foo, :bar)
+          assert_equal :bar, call_method(@object, meth_prefix, :instance_variable_get, :@foo)
+
+          call_method(@object, meth_prefix, :instance_variable_set, :@foo, :baz)
+          assert_equal :baz, call_method(@object, meth_prefix, :instance_variable_get, :@foo)
+        end
+
+        specify "reading and writing with a String name" do
+          call_method(@object, meth_prefix, :instance_variable_set, "@foo", :bar)
+          assert_equal :bar, call_method(@object, meth_prefix, :instance_variable_get, "@foo")
+        end
+
+        specify "reading with a Symbol and writing with a String name" do
+          call_method(@object, meth_prefix, :instance_variable_set, "@foo", :bar)
+          assert_equal :bar, call_method(@object, meth_prefix, :instance_variable_get, :@foo)
+        end
+
+        specify "reading with a String and writing with a Symbol name" do
+          call_method(@object, meth_prefix, :instance_variable_set, :@foo, :bar)
+          assert_equal :bar, call_method(@object, meth_prefix, :instance_variable_get, "@foo")
+        end
+
+        specify "reading an unset instance variable returns nil" do
+          assert_nil call_method(@object, meth_prefix, :instance_variable_get, :@foo)
+        end
       end
 
-      specify "reading with a Symbol and writing with a String name" do
-        call_method(@object, meth_prefix, :instance_variable_set, "@foo", :bar)
-        assert_equal :bar, call_method(@object, meth_prefix, :instance_variable_get, :@foo)
+      describe "#instance_variables" do
+        it "returns the names of existing instance variables" do
+          call_method(@object, meth_prefix, :instance_variable_set, :@bar, :foo)
+          call_method(@object, meth_prefix, :instance_variable_set, :@baz, :foo)
+
+          assert_equal [:@bar, :@baz], call_method(@object, meth_prefix, :instance_variables)
+        end
       end
 
-      specify "reading with a String and writing with a Symbol name" do
-        call_method(@object, meth_prefix, :instance_variable_set, :@foo, :bar)
-        assert_equal :bar, call_method(@object, meth_prefix, :instance_variable_get, "@foo")
+      describe "#instance_variable_defined?" do
+        it "returns true if instance variable is set" do
+          call_method(@object, meth_prefix, :instance_variable_set, :@bar, :foo)
+          assert call_method(@object, meth_prefix, :instance_variable_defined?, :@bar)
+        end
+
+        it "returns false if instance variable is not set" do
+          refute call_method(@object, meth_prefix, :instance_variable_defined?, :@bar)
+        end
       end
 
-      specify "reading an unset instance variable returns nil" do
-        assert_nil call_method(@object, meth_prefix, :instance_variable_get, :@foo)
-      end
-    end
+      describe "#remove_instance_variable" do
+        it "returns value of instance variable and unsets it" do
+          call_method(@object, meth_prefix, :instance_variable_set, :@bar, :foo)
+          assert call_method(@object, meth_prefix, :instance_variable_defined?, :@bar)
 
-    describe "#instance_variables" do
-      it "returns the names of existing instance variables" do
-        call_method(@object, meth_prefix, :instance_variable_set, :@bar, :foo)
-        call_method(@object, meth_prefix, :instance_variable_set, :@baz, :foo)
-
-        assert_equal [:@bar, :@baz], call_method(@object, meth_prefix, :instance_variables)
-      end
-    end
-
-    describe "#instance_variable_defined?" do
-      it "returns true if instance variable is set" do
-        call_method(@object, meth_prefix, :instance_variable_set, :@bar, :foo)
-        assert call_method(@object, meth_prefix, :instance_variable_defined?, :@bar)
+          assert_equal :foo, call_method(@object, meth_prefix, :remove_instance_variable, :@bar)
+          assert_nil call_method(@object, meth_prefix, :instance_variable_get, :@foo)
+          refute call_method(@object, meth_prefix, :instance_variable_defined?, :@foo)
+        end
       end
 
-      it "returns false if instance variable is not set" do
-        refute call_method(@object, meth_prefix, :instance_variable_defined?, :@bar)
-      end
-    end
-
-    describe "#remove_instance_variable" do
-      it "returns value of instance variable and unsets it" do
-        call_method(@object, meth_prefix, :instance_variable_set, :@bar, :foo)
-        assert call_method(@object, meth_prefix, :instance_variable_defined?, :@bar)
-
-        assert_equal :foo, call_method(@object, meth_prefix, :remove_instance_variable, :@bar)
-        assert_nil call_method(@object, meth_prefix, :instance_variable_get, :@foo)
-        refute call_method(@object, meth_prefix, :instance_variable_defined?, :@foo)
-      end
-    end
-
-    describe "getting an instance's class" do
-      specify "returns the class" do
-        assert_equal klass, call_method(@object, meth_prefix, :class)
+      describe "getting an instance's class" do
+        specify "returns the class" do
+          assert_equal klass, call_method(@object, meth_prefix, :class)
+        end
       end
     end
   end
