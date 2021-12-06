1d9225f7fc7291613355c568df526ede8a86ae10
Adrianna Chang <adrianna.chang@shopify.com>
2021-12-06 12:10:14 -0500

    Lint: use double quotes consistently

--- test/class_relationships_test.rb
+++ test/class_relationships_test.rb
@@ -1,77 +1,77 @@
-require 'new_impl/class_relationships'
-require 'test_helper'
+require "new_impl/class_relationships"
+require "test_helper"
 
 module NewImpl
   [
     [::Object, ::Module, ::Class],
-    [ToyObject, ToyModule, ToyClass, 'toy_']
+    [ToyObject, ToyModule, ToyClass, "toy_"]
   ].each do |_Object, _Module, _Class, prefix|
-    describe 'relationships between Object, Module and Class' do
+    describe "relationships between Object, Module and Class" do
       include TestHelpers
 
-      describe '#class' do
-        specify 'Object’s class is Class' do
+      describe "#class" do
+        specify "Object’s class is Class" do
           assert_equal _Class, call_method(_Object, prefix, :class)
         end
 
-        specify 'Module’s class is Class' do
+        specify "Module’s class is Class" do
           assert_equal _Class, call_method(_Module, prefix, :class)
         end
 
-        specify 'Class’s class is Class' do
+        specify "Class’s class is Class" do
           assert_equal _Class, call_method(_Class, prefix, :class)
         end
       end
 
-      describe '#superclass' do
-        specify 'Class’s superclass is Module' do
+      describe "#superclass" do
+        specify "Class’s superclass is Module" do
           assert_equal _Module, call_method(_Class, prefix, :superclass)
         end
 
-        specify 'Module’s superclass is Object' do
+        specify "Module’s superclass is Object" do
           assert_equal _Object, call_method(_Module, prefix, :superclass)
         end
       end
 
-      describe '#kind_of?' do
-        describe 'Object' do
-          specify 'Object is a kind of Object' do
+      describe "#kind_of?" do
+        describe "Object" do
+          specify "Object is a kind of Object" do
             assert call_method(_Object, prefix, :kind_of?, _Object)
           end
 
-          specify 'Object is a kind of Module' do
+          specify "Object is a kind of Module" do
             assert call_method(_Object, prefix, :kind_of?, _Module)
           end
 
-          specify 'Object is a kind of Class' do
+          specify "Object is a kind of Class" do
             assert call_method(_Object, prefix, :kind_of?, _Class)
           end
         end
 
-        describe 'Module' do
-          specify 'Module is a kind of Object' do
+        describe "Module" do
+          specify "Module is a kind of Object" do
             assert call_method(_Module, prefix, :kind_of?, _Object)
           end
 
-          specify 'Module is a kind of Module' do
+          specify "Module is a kind of Module" do
             assert call_method(_Module, prefix, :kind_of?, _Module)
           end
 
-          specify 'Module is a kind of Class' do
+          specify "Module is a kind of Class" do
             assert call_method(_Module, prefix, :kind_of?, _Class)
           end
         end
 
-        describe 'Class' do
-          specify 'Class is a kind of Object' do
+        describe "Class" do
+          specify "Class is a kind of Object" do
             assert call_method(_Class, prefix, :kind_of?, _Object)
           end
 
-          specify 'Class is a kind of Module' do
+          specify "Class is a kind of Module" do
             assert call_method(_Class, prefix, :kind_of?, _Module)
           end
 
-          specify 'Class is a kind of Class' do
+          specify "Class is a kind of Class" do
             assert call_method(_Class, prefix, :kind_of?, _Class)
           end
         end
--- test/toy_class_test.rb
+++ test/toy_class_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 require "new_impl/class_relationships"
 
-# require 'toy_class'
-# require 'toy_object'
+# require "toy_class"
+# require "toy_object"
 
 module NewImpl
   [
@@ -16,21 +16,21 @@ module NewImpl
         @class = call_method(klass, meth_prefix, :new)
       end
 
-      describe 'initializing an object' do
-        specify 'creates an instance of the class' do
+      describe "initializing an object" do
+        specify "creates an instance of the class" do
           instance = call_method(@class, meth_prefix, :new)
-          # Ask the instance what it's class is; it should be the anonymous
+          # Ask the instance what it"s class is; it should be the anonymous
           # Class stored in @class
           assert_equal @class, call_method(instance, meth_prefix, :class)
         end
 
-        specify 'creates an instance that acts like an object' do
+        specify "creates an instance that acts like an object" do
           skip "TO DO"
         end
       end
 
       describe "getting a class's superclass" do
-        specify 'returns the superclass' do
+        specify "returns the superclass" do
           assert_equal object_klass, call_method(@class, meth_prefix, :superclass)
         end
 
