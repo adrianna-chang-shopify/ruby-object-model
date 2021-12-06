ec2d381695afb0e1669edb63dc548bce735ddae0
Adrianna Chang <adrianna.chang@shopify.com>
2021-12-06 12:19:12 -0500

    Move implementations out to unique files + update class relationship tests

--- lib/toy_class.rb
+++ lib/toy_class.rb
@@ -1,21 +1,126 @@
-require "toy_module"
 require "toy_object"
 
-class ToyClass < ToyModule
-  def self.toy_new(toy_superclass = ToyObject)
-    new(self, toy_superclass)
+ToyClass = BasicObject.new
+
+def ToyClass.toy_new
+  instance = BasicObject.new
+
+  klass = self
+
+  singleton_class = (class << instance; self; end)
+  singleton_class.define_method(:toy_class) { klass }
+
+  class << instance
+    # Object instance methods
+
+    def ivar_map
+      @ivar_map ||= {}
+    end
+
+    def toy_instance_variable_get(name)
+      ivar_map[name.to_sym]
+    end
+
+    def toy_instance_variable_set(name, value)
+      ivar_map[name.to_sym] = value
+    end
+
+    def toy_instance_variables
+      ivar_map.keys
+    end
+
+    def toy_instance_variable_defined?(name)
+      ivar_map.has_key?(name.to_sym)
+    end
+
+    def toy_remove_instance_variable(name)
+      ivar_map.delete(name.to_sym)
+    end
+
+    # Module instance methods
+
+    def constant_map
+      @constant_map ||= {}
+    end
+
+    def toy_const_get(name)
+      name = name.to_sym
+      raise NameError, "uninitialized constant #{name} for #{inspect}" unless constant_map.key?(name)
+
+      constant_map[name]
+    end
+
+    def toy_const_set(name, value)
+      name = name.to_sym
+      raise NameError unless name.match?(/^[A-Z][a-zA-Z_]*$/)
+
+      ::Kernel.warn "already initialized constant #{name}" if constant_map.key?(name)
+
+      constant_map[name] = value
+    end
+
+    def toy_constants
+      constant_map.keys.map(&:to_sym)
+    end
+
+    def method_map
+      @method_map ||= {}
+    end
+
+    def toy_define_method(name, method)
+      name = name.to_sym
+      method_map[name] = method
+    end
+
+    def toy_instance_methods
+      method_map.keys.map(&:to_sym)
+    end
+
+    # Class instance methods
+    def toy_new
+      instance = BasicObject.new
+
+      # self is our anonymous class
+      # In Ruby, this would look like #<Class:0x00007fae319f3bc0>
+      klass = self
+
+      # singleton class is the singleton class of the instance of the anonymous class
+      singleton_class = (class << instance; self; end)
+      singleton_class.define_method(:toy_class) { klass }
+
+      #  TO DO: Make this instance act like a proper ToyObject
+      instance
+    end
+
+    def toy_superclass
+      ::ToyObject
+    end
   end
 
-  def initialize(toy_class, toy_superclass)
-    @toy_superclass = toy_superclass
-    super(toy_class)
-  end
+  instance
+end
 
-  def toy_new
-    ToyObject.new(self)
-  end
+def ToyClass.to_s
+  inspect
+end
 
-  def toy_superclass
-    @toy_superclass
+def ToyClass.inspect
+  "ToyClass"
+end
+
+def ToyClass.toy_class
+  ToyClass
+end
+
+def ToyClass.toy_superclass
+  ToyModule
+end
+
+def ToyClass.toy_kind_of?(klass)
+  superclass = toy_class
+  while superclass
+    return true if klass == superclass
+    superclass = superclass.toy_superclass
   end
+  false
 end
--- test/class_relationships_test.rb
+++ test/class_relationships_test.rb
@@ -1,79 +1,78 @@
-require "new_impl/class_relationships"
+require "toy_class"
+require "toy_module"
+require "toy_object"
 require "test_helper"
+[
+  [::Object, ::Module, ::Class],
+  [ToyObject, ToyModule, ToyClass, "toy_"]
+].each do |_Object, _Module, _Class, prefix|
+  describe "relationships between Object, Module and Class" do
+    include TestHelpers
 
-module NewImpl
-  [
-    [::Object, ::Module, ::Class],
-    [ToyObject, ToyModule, ToyClass, "toy_"]
-  ].each do |_Object, _Module, _Class, prefix|
-    describe "relationships between Object, Module and Class" do
-      include TestHelpers
+    describe "#class" do
+      specify "Object’s class is Class" do
+        assert_equal _Class, call_method(_Object, prefix, :class)
+      end
 
-      describe "#class" do
-        specify "Object’s class is Class" do
-          assert_equal _Class, call_method(_Object, prefix, :class)
+      specify "Module’s class is Class" do
+        assert_equal _Class, call_method(_Module, prefix, :class)
+      end
+
+      specify "Class’s class is Class" do
+        assert_equal _Class, call_method(_Class, prefix, :class)
+      end
+    end
+
+    describe "#superclass" do
+      specify "Class’s superclass is Module" do
+        assert_equal _Module, call_method(_Class, prefix, :superclass)
+      end
+
+      specify "Module’s superclass is Object" do
+        assert_equal _Object, call_method(_Module, prefix, :superclass)
+      end
+    end
+
+    describe "#kind_of?" do
+      describe "Object" do
+        specify "Object is a kind of Object" do
+          assert call_method(_Object, prefix, :kind_of?, _Object)
         end
 
-        specify "Module’s class is Class" do
-          assert_equal _Class, call_method(_Module, prefix, :class)
+        specify "Object is a kind of Module" do
+          assert call_method(_Object, prefix, :kind_of?, _Module)
         end
 
-        specify "Class’s class is Class" do
-          assert_equal _Class, call_method(_Class, prefix, :class)
+        specify "Object is a kind of Class" do
+          assert call_method(_Object, prefix, :kind_of?, _Class)
         end
       end
 
-      describe "#superclass" do
-        specify "Class’s superclass is Module" do
-          assert_equal _Module, call_method(_Class, prefix, :superclass)
+      describe "Module" do
+        specify "Module is a kind of Object" do
+          assert call_method(_Module, prefix, :kind_of?, _Object)
         end
 
-        specify "Module’s superclass is Object" do
-          assert_equal _Object, call_method(_Module, prefix, :superclass)
+        specify "Module is a kind of Module" do
+          assert call_method(_Module, prefix, :kind_of?, _Module)
+        end
+
+        specify "Module is a kind of Class" do
+          assert call_method(_Module, prefix, :kind_of?, _Class)
         end
       end
 
-      describe "#kind_of?" do
-        describe "Object" do
-          specify "Object is a kind of Object" do
-            assert call_method(_Object, prefix, :kind_of?, _Object)
-          end
-
-          specify "Object is a kind of Module" do
-            assert call_method(_Object, prefix, :kind_of?, _Module)
-          end
-
-          specify "Object is a kind of Class" do
-            assert call_method(_Object, prefix, :kind_of?, _Class)
-          end
+      describe "Class" do
+        specify "Class is a kind of Object" do
+          assert call_method(_Class, prefix, :kind_of?, _Object)
         end
 
-        describe "Module" do
-          specify "Module is a kind of Object" do
-            assert call_method(_Module, prefix, :kind_of?, _Object)
-          end
-
-          specify "Module is a kind of Module" do
-            assert call_method(_Module, prefix, :kind_of?, _Module)
-          end
-
-          specify "Module is a kind of Class" do
-            assert call_method(_Module, prefix, :kind_of?, _Class)
-          end
+        specify "Class is a kind of Module" do
+          assert call_method(_Class, prefix, :kind_of?, _Module)
         end
 
-        describe "Class" do
-          specify "Class is a kind of Object" do
-            assert call_method(_Class, prefix, :kind_of?, _Object)
-          end
-
-          specify "Class is a kind of Module" do
-            assert call_method(_Class, prefix, :kind_of?, _Module)
-          end
-
-          specify "Class is a kind of Class" do
-            assert call_method(_Class, prefix, :kind_of?, _Class)
-          end
+        specify "Class is a kind of Class" do
+          assert call_method(_Class, prefix, :kind_of?, _Class)
         end
       end
     end
