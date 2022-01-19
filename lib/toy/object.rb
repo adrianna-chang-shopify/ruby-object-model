92f65dd5e05a1773430bf20fdad9e91a5c78ce78
Adrianna Chang <adrianna.chang@shopify.com>
2022-01-19 12:09:16 -0500

    Extract ObjectInstance

--- lib/toy/object.rb
+++ lib/toy/object.rb
@@ -1,4 +1,5 @@
 require "toy/behaviours"
+require "toy/object_instance"
 
 module Toy
   Object = BasicObject.new
@@ -33,25 +34,6 @@ module Toy
 
     def new
       ObjectInstance.new(self)
-    end
-  end
-
-  class ObjectInstance < BasicObject
-    # Object instance methods
-    include Behaviours::InstanceVariables
-
-    # kind_of?
-    include Behaviours::ClassRelationships
-
-    # to_s and #inspect
-    include Behaviours::Inspection
-
-    def initialize(klass)
-      @klass = klass
-    end
-
-    def class
-      @klass
     end
   end
 end
---
+++ lib/toy/object_instance.rb
@@ -0,0 +1,22 @@
+require "toy/behaviours"
+
+module Toy
+  class ObjectInstance < BasicObject
+    # Object instance methods
+    include Behaviours::InstanceVariables
+
+    # kind_of?
+    include Behaviours::ClassRelationships
+
+    # to_s and #inspect
+    include Behaviours::Inspection
+
+    def initialize(klass)
+      @klass = klass
+    end
+
+    def class
+      @klass
+    end
+  end
+end
