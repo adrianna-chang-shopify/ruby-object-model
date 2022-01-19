124f40df7a05c67708b49a58bde93ec260382257
Adrianna Chang <adrianna.chang@shopify.com>
2022-01-19 12:01:34 -0500

    Extract Class instance creation into Ruby class

--- lib/toy/class.rb
+++ lib/toy/class.rb
@@ -1,4 +1,5 @@
 require "toy/behaviours"
+require "toy/module"
 require "toy/object"
 
 module Toy
@@ -33,53 +34,41 @@ module Toy
     end
 
     def new(superclass = Object)
+      ClassInstance.new(self, superclass)
+    end
+  end
+
+  class ClassInstance < ModuleInstance
+    def initialize(klass, superclass)
+      super(klass)
+      @superclass = superclass
+    end
+
+    def superclass
+      @superclass
+    end
+
+    # Class instance methods
+    def new
       instance = BasicObject.new
 
+      # self is our anonymous class
+      # In Ruby, this would look like #<Class:0x00007fae319f3bc0>
       klass = self
 
+      # singleton_class is the singleton class of the instance of the anonymous class
       singleton_class = (class << instance; self; end)
       singleton_class.define_method(:class) { klass }
-      singleton_class.define_method(:superclass) { superclass }
 
       class << instance
         # Object instance methods
         include Behaviours::InstanceVariables
-
-        # Module instance methods
-        include Behaviours::Constants
-        include Behaviours::Methods
 
         # kind_of?
         include Behaviours::ClassRelationships
 
         # to_s and #inspect
         include Behaviours::Inspection
-
-        # Class instance methods
-        def new
-          instance = BasicObject.new
-
-          # self is our anonymous class
-          # In Ruby, this would look like #<Class:0x00007fae319f3bc0>
-          klass = self
-
-          # singleton_class is the singleton class of the instance of the anonymous class
-          singleton_class = (class << instance; self; end)
-          singleton_class.define_method(:class) { klass }
-
-          class << instance
-            # Object instance methods
-            include Behaviours::InstanceVariables
-
-            # kind_of?
-            include Behaviours::ClassRelationships
-
-            # to_s and #inspect
-            include Behaviours::Inspection
-          end
-
-          instance
-        end
       end
 
       instance
