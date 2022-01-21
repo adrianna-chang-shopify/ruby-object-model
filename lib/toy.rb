a639ce03a038da1f4b4e9445dcc35c6c95b4b612
Adrianna Chang <adrianna.chang@shopify.com>
2022-01-21 14:54:20 -0500

    Toy module autoloads all other constants

---
+++ lib/toy.rb
@@ -0,0 +1,9 @@
+module Toy
+  autoload(:Behaviours, "toy/behaviours.rb")
+  autoload(:ClassInstance, "toy/class_instance.rb")
+  autoload(:Class, "toy/class.rb")
+  autoload(:ModuleInstance, "toy/module_instance.rb")
+  autoload(:Module, "toy/module.rb")
+  autoload(:ObjectInstance, "toy/object_instance.rb")
+  autoload(:Object, "toy/object.rb")
+end
