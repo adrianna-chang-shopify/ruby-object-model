012d51de21e1c336f3c2f3fda6a1475d664e8ed6
Tom Stuart <tom.stuart@shopify.com>
2022-01-24 12:11:25 +0000

    Extract tests into modules
    This creates the opportunity for us to reuse these tests for other
    objects if we want to.
    
    For reasons I don’t fully understand, this doesn’t work right:
    
    ```
    module FooBehaviourTests
      describe 'intrinsic fooness' do
        it 'is fooey' do
          assert @object.fooey?
        end
      end
    end
    
    describe Foo do
      before do
        @object = Foo.new
      end
    
      include FooBehaviourTests
    end
    ```
    
    Here `@object` is always `nil` during the test, presumably for
    mysterious reasons relating to evaluation order. What I do know is that
    generating the tests within the module’s `#included` hook makes the
    problem go away…
    
    ```
    module FooBehaviourTests
      def included(_)
        describe 'intrinsic fooness' do
          it 'is fooey' do
            assert @object.fooey?
          end
        end
      end
    end
    
    describe Foo do
      before do
        @object = Foo.new
      end
    
      include FooBehaviourTests
    end
    ```
    
    …so that’s what I’ve done. 🤷

--- test/class_test.rb
+++ test/class_test.rb
@@ -1,7 +1,79 @@
 require "test_helper"
 
+module ClassBehaviourTests
+  def self.included(_)
+    describe "Class-like behaviour" do
+      describe "initializing an object" do
+        before do
+          @object = @class.new
+        end
+
+        specify "creates an instance of the class" do
+          # Ask the instance what its class is; it should be the anonymous
+          # Class stored in @class
+          assert_equal @class, @object.class
+        end
+
+        describe "acts like an object" do
+          describe "accessing instance variables" do
+            specify "reading and writing" do
+              @object.instance_variable_set(:@foo, :bar)
+              assert_equal :bar, @object.instance_variable_get(:@foo)
+
+              @object.instance_variable_set(:@foo, :baz)
+              assert_equal :baz, @object.instance_variable_get(:@foo)
+            end
+          end
+
+          describe "#instance_variables" do
+            it "returns the names of existing instance variables" do
+              @object.instance_variable_set(:@bar, :foo)
+              @object.instance_variable_set(:@baz, :foo)
+
+              assert_equal [:@bar, :@baz], @object.instance_variables
+            end
+          end
+
+          describe "#instance_variable_defined?" do
+            it "returns true if instance variable is set" do
+              @object.instance_variable_set(:@bar, :foo)
+              assert @object.instance_variable_defined?(:@bar)
+            end
+
+            it "returns false if instance variable is not set" do
+              refute @object.instance_variable_defined?(:@bar)
+            end
+          end
+
+          describe "#remove_instance_variable" do
+            it "returns value of instance variable and unsets it" do
+              @object.instance_variable_set(:@bar, :foo)
+              assert @object.instance_variable_defined?(:@bar)
+
+              assert_equal :foo, @object.remove_instance_variable(:@bar)
+              assert_nil @object.instance_variable_get(:@foo)
+              refute @object.instance_variable_defined?(:@foo)
+            end
+          end
+        end
+      end
+
+      describe "getting a class's superclass" do
+        specify "returns the superclass" do
+          assert_equal @superclass, @class.superclass
+        end
+
+        specify "works with custom superclass" do
+          subclass = @Class.new(@class)
+          assert_equal @class, subclass.superclass
+        end
+      end
+    end
+  end
+end
+
 [::Object, ::Toy].each do |ns|
-  describe "Class-like behaviour in the #{ns} namespace" do
+  describe "in the #{ns} namespace" do
     [[ns::Class, ns::Object]].each do |_Class, _Object|
       describe _Class do
         before do
@@ -10,71 +82,7 @@ require "test_helper"
           @class = _Class.new
         end
 
-        describe "initializing an object" do
-          before do
-           @object = @class.new
-          end
-
-          specify "creates an instance of the class" do
-            # Ask the instance what its class is; it should be the anonymous
-            # Class stored in @class
-            assert_equal @class, @object.class
-          end
-
-          describe "acts like an object" do
-            describe "accessing instance variables" do
-              specify "reading and writing" do
-                @object.instance_variable_set(:@foo, :bar)
-                assert_equal :bar, @object.instance_variable_get(:@foo)
-
-                @object.instance_variable_set(:@foo, :baz)
-                assert_equal :baz, @object.instance_variable_get(:@foo)
-              end
-            end
-
-            describe "#instance_variables" do
-              it "returns the names of existing instance variables" do
-                @object.instance_variable_set(:@bar, :foo)
-                @object.instance_variable_set(:@baz, :foo)
-
-                assert_equal [:@bar, :@baz], @object.instance_variables
-              end
-            end
-
-            describe "#instance_variable_defined?" do
-              it "returns true if instance variable is set" do
-                @object.instance_variable_set(:@bar, :foo)
-                assert @object.instance_variable_defined?(:@bar)
-              end
-
-              it "returns false if instance variable is not set" do
-                refute @object.instance_variable_defined?(:@bar)
-              end
-            end
-
-            describe "#remove_instance_variable" do
-              it "returns value of instance variable and unsets it" do
-                @object.instance_variable_set(:@bar, :foo)
-                assert @object.instance_variable_defined?(:@bar)
-
-                assert_equal :foo, @object.remove_instance_variable(:@bar)
-                assert_nil @object.instance_variable_get(:@foo)
-                refute @object.instance_variable_defined?(:@foo)
-              end
-            end
-          end
-        end
-
-        describe "getting a class's superclass" do
-          specify "returns the superclass" do
-            assert_equal @superclass, @class.superclass
-          end
-
-          specify "works with custom superclass" do
-            subclass = @Class.new(@class)
-            assert_equal @class, subclass.superclass
-          end
-        end
+        include ClassBehaviourTests
       end
     end
   end
