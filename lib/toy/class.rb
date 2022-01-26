1d41cb776681b149539487a6cfa22b979f5052b5
Adrianna Chang <adrianna.chang@shopify.com>
2022-01-26 11:59:08 -0500

    Toy::Class is a ClassInstance

--- lib/toy/class.rb
+++ lib/toy/class.rb
@@ -1,32 +1,14 @@
 module Toy
-  Class = BasicObject.new
+  Class = ClassInstance.new(superclass: Module)
 
   # Open up singleton class of Toy::Class
   class << Class
-    # Object instance methods, because the Class singleton is an object yo!
-    include Behaviours::InstanceVariables
-
-    # Module instance methods, because the Class singleton is a module yo!
-    include Behaviours::Constants
-    include Behaviours::Methods
-
-    # kind_of?
-    include Behaviours::ClassRelationships
-
-    def to_s
-      inspect
-    end
-
     def inspect
       "Toy::Class"
     end
 
     def class
       Class
-    end
-
-    def superclass
-      Module
     end
 
     def new(superclass = Object)
