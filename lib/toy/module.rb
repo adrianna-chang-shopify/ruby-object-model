56dc7c9d8204915a877e31ab05058afc045e8c31
Adrianna Chang <adrianna.chang@shopify.com>
2022-01-26 12:00:21 -0500

    Module is a ClassInstance

--- lib/toy/module.rb
+++ lib/toy/module.rb
@@ -1,32 +1,14 @@
 module Toy
-  Module = BasicObject.new
+  Module = ClassInstance.new(superclass: Object)
 
   # Open up singleton class of Toy::Module
   class << Module
-    # Object instance methods, because the Module singleton is an object yo!
-    include Behaviours::InstanceVariables
-
-    # Module instance methods, because the Module singleton is a module yo!
-    include Behaviours::Constants
-    include Behaviours::Methods
-
-    # kind_of?
-    include Behaviours::ClassRelationships
-
-    def to_s
-      inspect
-    end
-
     def inspect
       "Toy::Module"
     end
 
     def class
       Class
-    end
-
-    def superclass
-      Object
     end
 
     def new
