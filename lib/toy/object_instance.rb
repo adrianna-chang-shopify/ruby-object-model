c683cafd9961b1e24ed4418aef3165f07c8a706b
Adrianna Chang <adrianna.chang@shopify.com>
2022-03-02 13:09:51 -0500

    Check mixed-in modules on superclasses to see if they define methods.

--- lib/toy/object_instance.rb
+++ lib/toy/object_instance.rb
@@ -47,13 +47,17 @@ module Toy
 
     def method(selector)
       superclass = self.class
-      # Check mixed-in Modules to see if they define the selector
-      superclass.included_modules.each do |mixin|
-        return Method.new(mixin) if mixin.instance_methods.include?(selector)
-      end
-      # Check up the superclass hierarchy to see if any of them define the selector
+
+      # Check up the superclass hierarchy to see if any of them define the selector.
+      # Check the superclasses to see if any of their mixins define the selector.
       while superclass
         return Method.new(superclass) if superclass.instance_methods.include?(selector)
+
+        # Check mixed-in Modules to see if they define the selector
+        superclass.included_modules.each do |mixin|
+          return Method.new(mixin) if mixin.instance_methods.include?(selector)
+        end
+
         superclass = superclass.superclass
       end
       ::Kernel.raise ::NameError, "undefined method `#{selector}' for class `#{self.class.inspect}'"
--- test/mixin_test.rb
+++ test/mixin_test.rb
@@ -14,6 +14,7 @@ class MixinTest
       end
 
       describe "including a module in a module" do
+        # TO DO - ensure we test this behaviour against Module
       end
 
       describe "including a module in a class" do
@@ -38,6 +39,33 @@ class MixinTest
 
           m.define_method(:my_method, proc { puts "Hello World!" })
           assert_equal(m, o.method(:my_method).owner)
+        end
+
+        specify "mixes in behaviour to a subclass" do
+          m = ns::Module.new
+          m.define_method(:my_method, proc { puts "Hello World!" })
+
+          c = ns::Class.new
+          c.include(m)
+
+          subclass = ns::Class.new(c)
+
+          o = subclass.new
+
+          assert_equal(m, o.method(:my_method).owner)
+        end
+
+        specify "when a class overrides a mixin's method, knows the class defined it" do
+          m = ns::Module.new
+          m.define_method(:my_method, proc { puts "Hello World!" })
+
+          c = ns::Class.new
+          c.include(m)
+          c.define_method(:my_method, proc { puts "I override my mixin's method!" })
+
+          o = c.new
+
+          assert_equal(c, o.method(:my_method).owner)
         end
       end
     end
