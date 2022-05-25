eb7a93fbf60dfa707c377880148c3b12096b0b52
Adrianna Chang <adrianna.chang@shopify.com>
2022-05-25 12:24:50 -0400

    Clean up ModuleInstance#instance_method

--- lib/toy/module_instance.rb
+++ lib/toy/module_instance.rb
@@ -40,19 +40,18 @@ module Toy
     end
 
     def instance_method(selector)
-      selector = selector.to_sym
+      return Method.new(self) if instance_methods.include?(selector)
 
-      current_class = self
-      while current_class
-        # Check mixed-in modules to see if they define the selector
-        method = mixin_ancestry_search(current_class, selector)
+      included_modules.each do |nested_mixin|
+        method = nested_mixin.instance_method(selector)
         return method if method
-
-        # The current class did not define the method, nor did any of its mixins.
-        # Move up to the superclass to see if it, or any of its modules, contain the method.
-        current_class = current_class.superclass
       end
-      ::Kernel.raise ::NameError, "undefined method `#{selector}' for class `#{self.inspect}'"
+
+      if superclass
+        superclass.instance_method(selector)
+      else
+        ::Kernel.raise ::NameError, "undefined method `#{selector}' for class `#{self.inspect}'"
+      end
     end
 
     def include(mod)
@@ -71,18 +70,6 @@ module Toy
 
     def method_map
       @method_map ||= {}
-    end
-
-    def mixin_ancestry_search(mixin, selector)
-      return Method.new(mixin) if mixin.instance_methods.include?(selector)
-
-      mixin.included_modules.each do |nested_mixin|
-        method = mixin_ancestry_search(nested_mixin, selector)
-        return method if method
-      end
-
-      # No method match was found at this level in the tree, return nil
-      nil
     end
   end
 end
